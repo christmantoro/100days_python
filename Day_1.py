f2e0edf9d598ddec868bf956235c801379957d43
Anthony Christmantoro <christmantoro@gmail.com>
2024-01-01 15:36:50 +0700

    Hari Pertama

---
+++ Day_1.py
@@ -0,0 +1,5 @@
+print ("selamat datang di 100 days of python")
+mengapa = input ("mengapa kamu ingin kamu belajar python?")
+bagaiamana = input ("bagaimana agar kamu konsisten belajar python?")
+
+print ("jadi ini adalah alasan kamu belajar python", mengapa+" dengan cara  "+bagaiamana)
